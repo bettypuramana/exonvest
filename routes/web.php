9d9fd4e13d8c9c571580121c0aacb012a9e12cde
Betty <bettypuramana@gmail.com>
2024-12-12 18:59:47 +0530

    langauge set

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::middleware('setLocale')->prefix($locale)->group(function() {
 Route::get('/', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user.home');
 Route::get('/about', [App\Http\Controllers\User\HomeController::class, 'about_us'])->name('user.about');
 Route::get('/contact_us', [App\Http\Controllers\User\HomeController::class, 'contact_us'])->name('user.contact_us');
 Route::get('/blog', [App\Http\Controllers\User\HomeController::class, 'blog'])->name('user.blog');
 Route::get('/blog_detail', [App\Http\Controllers\User\HomeController::class, 'blog_detail'])->name('user.blog_detail');
 Route::get('/careers', [App\Http\Controllers\User\HomeController::class, 'careers'])->name('user.careers');
-
+});
